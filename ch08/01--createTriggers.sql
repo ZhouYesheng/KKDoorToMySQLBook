a26e975e58508dcbc74f212b4d40bf54b32bbe99
DESKTOP-6POJJMP\kiky <huafeihua123xxx@gmail.com>
2017-10-16 07:36:03 +0800

    创建多执行触发器

--- ch08/01--createTriggers.sql
+++ ch08/01--createTriggers.sql
@@ -24,3 +24,18 @@ CREATE TRIGGER tri_diarytime
 
 #插入新的部门表记录---》触发了tri_diarytime
 INSERT INTO t_dept VALUES(1,'cjgongdept','ShangXi');
+
+
+#创建多执行触发器
+DELIMITER $$
+CREATE TRIGGER tri_diarytime2
+	AFTER INSERT ON t_dept FOR EACH ROW
+		BEGIN
+			INSERT INTO t_diary(tablename,diarytime) VALUES('t_dept',NOW());
+			INSERT INTO t_diary(tablename,diarytime) VALUES('t_dept',NOW());
+		END $$
+#恢复定界符
+DELIMITER ;
+
+#插入新的部门表记录---》触发了tri_diarytime2、tri_diarytime1
+INSERT INTO t_dept VALUES(2,'cjgongdept2','ShangXi2');
