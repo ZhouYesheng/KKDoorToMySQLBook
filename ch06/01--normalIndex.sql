d36c37601d9aaebdfe439e16c2c8665144d43a6f
DESKTOP-6POJJMP\kiky <huafeihua123xxx@gmail.com>
2017-10-15 11:36:12 +0800

    在普通表创建普通索引

--- ch06/01--normalIndex.sql
+++ ch06/01--normalIndex.sql
@@ -1,7 +1,12 @@
 ####
 #普通索引
 ###
+#删除部门表
+DROP TABLE t_dept;
 
+#检查是否启用了索引
+EXPLAIN
+	SELECT * FROM t_dept WHERE deptno=1;
 
 #创建部门表，并且以主键deptno建立索引
 CREATE TABLE t_dept(
@@ -13,6 +18,13 @@ CREATE TABLE t_dept(
 
 
 
-#检查是否启用了索引
-EXPLAIN
-	SELECT * FROM t_dept WHERE deptno=1;
+#创建无索引的普通表
+CREATE TABLE t_dept(
+	deptno INT,
+	dname VARCHAR(20),
+	loc VARCHAR(40)
+);
+
+#在普通表上建立索引
+CREATE INDEX index_deptno
+	ON t_dept(deptno);
