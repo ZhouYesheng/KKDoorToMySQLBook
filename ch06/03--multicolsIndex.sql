5c7173ed6fbd4855ea792a9b454ae4a65fccc729
DESKTOP-6POJJMP\kiky <huafeihua123xxx@gmail.com>
2017-10-15 15:43:34 +0800

    创建表的时候使用多列索引

---
+++ ch06/03--multicolsIndex.sql
@@ -0,0 +1,27 @@
+####
+#多列索引
+###
+
+#创建无索引部门表
+CREATE TABLE t_dept(
+	deptno INT,
+	dname VARCHAR(20),
+	loc VARCHAR(40)
+);
+
+
+#删除部门表
+DROP TABLE t_dept;
+
+#检查是否启用了索引
+EXPLAIN
+	SELECT * FROM t_dept WHERE deptno=1;
+
+
+#创建表的时候使用多可索引
+CREATE TABLE t_dept(
+	deptno INT,
+	dname VARCHAR(20),
+	loc VARCHAR(40),
+	INDEX index_dname_loc(dname,loc)
+);
