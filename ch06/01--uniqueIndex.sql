8cb6b3c177754d944622d5ad3bf21ba1f2abab42
DESKTOP-6POJJMP\kiky <huafeihua123xxx@gmail.com>
2017-10-15 11:50:05 +0800

    修改表时候创建唯一索引

--- ch06/01--uniqueIndex.sql
+++ ch06/01--uniqueIndex.sql
@@ -1,6 +1,15 @@
 ####
 #普通索引
 ###
+
+#创建无索引部门表
+CREATE TABLE t_dept(
+	deptno INT,
+	dname VARCHAR(20),
+	loc VARCHAR(40)
+);
+
+
 #删除部门表
 DROP TABLE t_dept;
 
@@ -17,14 +26,11 @@ CREATE TABLE t_dept(
 );
 
 
-#创建无索引部门表
-CREATE TABLE t_dept(
-	deptno INT,
-	dname VARCHAR(20),
-	loc VARCHAR(40)
-);
-
 #在已经存在的表上创建唯一索引
 CREATE UNIQUE INDEX index_dept
 	ON t_dept(deptno);
+
+#修改表时候创建唯一索引
+ALTER TABLE t_dept
+	ADD UNIQUE INDEX index_deptno(deptno);
 
