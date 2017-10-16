5144041e475fdb78db8a08c3a640e4750a01a49c
DESKTOP-6POJJMP\kiky <huafeihua123xxx@gmail.com>
2017-10-17 07:24:45 +0800

    更新所有记录

--- ch09/02--updateRecords.sql
+++ ch09/02--updateRecords.sql
@@ -9,3 +9,11 @@ SELECT * FROM t_dept;
 #更新特定记录
 UPDATE t_dept
 	SET loc='ShangeHai2' WHERE dname='cjgong1';
+
+#更新所有记录
+UPDATE t_dept
+    SET loc='ShangeHai8' WHERE deptno<=6;
+
+#无条件更新所有记录
+UPDATE t_dept
+    SET loc='ShangeHai9';
