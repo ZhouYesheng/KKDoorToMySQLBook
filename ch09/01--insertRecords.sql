87f7e0a018fa6595daac5773b61446940d22f01a
DESKTOP-6POJJMP\kiky <huafeihua123xxx@gmail.com>
2017-10-16 08:04:35 +0800

    插入完整记录

---
+++ ch09/01--insertRecords.sql
@@ -0,0 +1,12 @@
+####
+#插入记录
+###
+
+#查询部门表
+SELECT * FROM t_dept;
+
+#插入完整记录
+INSERT INTO t_dept(deptno,dname,loc) VALUES(3,'cjgong3','ShangeHai3');
+
+#简略版插入完整记录
+INSERT INTO t_dept VALUES(4,'cjgong4','ShangeHai4');
