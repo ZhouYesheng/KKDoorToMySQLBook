317b1ef3494d3f63e1b134cf49b1da460bafd360
DESKTOP-6POJJMP\kiky <huafeihua123xxx@gmail.com>
2017-10-16 08:21:09 +0800

    插入部分记录

--- ch09/01--insertRecords.sql
+++ ch09/01--insertRecords.sql
@@ -10,3 +10,6 @@ INSERT INTO t_dept(deptno,dname,loc) VALUES(3,'cjgong3','ShangeHai3');
 
 #简略版插入完整记录
 INSERT INTO t_dept VALUES(4,'cjgong4','ShangeHai4');
+
+#插入部分记录
+INSERT INTO t_dept(dname,loc) VALUES('cjgong5','ShangeHai5');
