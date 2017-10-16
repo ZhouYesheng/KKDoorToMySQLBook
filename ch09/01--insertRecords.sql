b6469b23880e49d4476142e415ab6e5df4e0ef1c
DESKTOP-6POJJMP\kiky <huafeihua123xxx@gmail.com>
2017-10-16 08:28:49 +0800

    插入多条完整记录

--- ch09/01--insertRecords.sql
+++ ch09/01--insertRecords.sql
@@ -5,6 +5,9 @@
 #查询部门表
 SELECT * FROM t_dept;
 
+#删除所有记录
+DELETE FROM t_dept;
+
 #插入完整记录
 INSERT INTO t_dept(deptno,dname,loc) VALUES(3,'cjgong3','ShangeHai3');
 
@@ -13,3 +16,19 @@ INSERT INTO t_dept VALUES(4,'cjgong4','ShangeHai4');
 
 #插入部分记录
 INSERT INTO t_dept(dname,loc) VALUES('cjgong5','ShangeHai5');
+
+#插入多条完整记录
+INSERT INTO t_dept(deptno,dname,loc)
+	VALUES(1,'cjgong1','ShangeHai1'),
+		(2,'cjgong2','ShangeHai2'),
+		(3,'cjgong3','ShangeHai3'),
+		(4,'cjgong4','ShangeHai4'),
+		(5,'cjgong5','ShangeHai5');
+
+#简略版插入多条完整记录
+INSERT INTO t_dept
+	VALUES(1,'cjgong1','ShangeHai1'),
+		(2,'cjgong2','ShangeHai2'),
+		(3,'cjgong3','ShangeHai3'),
+		(4,'cjgong4','ShangeHai4'),
+		(5,'cjgong5','ShangeHai5');
