6797ba734c02b75939e056dae8d40aec87cddd92
DESKTOP-6POJJMP\kiky <huafeihua123xxx@gmail.com>
2017-10-18 07:48:59 +0800

    模糊查询

--- ch10/03--queryOnCondi.sql
+++ ch10/03--queryOnCondi.sql
@@ -28,5 +28,44 @@ SELECT ename FROM t_employee WHERE NOT sal IS NULL;
 #集合内查询
 SELECT ename FROM t_employee WHERE empno IN (7521,7782,7566,7788);
 
+#集合内查询（NULL元素自动忽略）
+SELECT ename FROM t_employee WHERE empno IN (7521,7782,7566,7788,NULL);
+
 #集合外查询
 SELECT ename FROM t_employee WHERE empno NOT IN (7521,7782,7566,7788);
+
+
+#%模糊查询
+SELECT ename FROM t_employee
+	WHERE ename LIKE "A%";
+
+#%模糊查询取反
+SELECT ename FROM t_employee
+	WHERE NOT ename LIKE "A%";
+
+
+#_模糊查询
+#查询名字的第二个字母是A的员工
+SELECT ename FROM t_employee
+	WHERE ename LIKE "_A%";
+
+
+#模糊取反查询
+SELECT ename FROM t_employee
+	WHERE ename NOT LIKE "_A%";
+
+
+#模糊所有
+SELECT ename FROM t_employee
+	WHERE ename LIKE "%%";
+
+
+
+
+
+
+
+
+
+
+
