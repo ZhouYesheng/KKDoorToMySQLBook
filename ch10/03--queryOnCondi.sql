964f364988337e1e8d2bc76de4520703f917894c
DESKTOP-6POJJMP\kiky <huafeihua123xxx@gmail.com>
2017-10-18 07:20:36 +0800

    范围查询

--- ch10/03--queryOnCondi.sql
+++ ch10/03--queryOnCondi.sql
@@ -8,3 +8,10 @@ SELECT ename FROM t_employee WHERE job="CLERK";
 
 #多条件查询
 SELECT ename FROM t_employee WHERE job="CLERK" && sal>800;
+
+#指定范围查询
+SELECT ename FROM t_employee WHERE sal BETWEEN 1000 AND 2000;
+
+#范围外查询
+SELECT ename FROM t_employee WHERE sal NOT BETWEEN 1000 AND 2000;
+
