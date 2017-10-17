5fc3be5c97178bad001e6cf54f7236c7bae630be
DESKTOP-6POJJMP\kiky <huafeihua123xxx@gmail.com>
2017-10-18 07:28:20 +0800

    空值查询

--- ch10/03--queryOnCondi.sql
+++ ch10/03--queryOnCondi.sql
@@ -15,3 +15,12 @@ SELECT ename FROM t_employee WHERE sal BETWEEN 1000 AND 2000;
 #范围外查询
 SELECT ename FROM t_employee WHERE sal NOT BETWEEN 1000 AND 2000;
 
+
+#空值查询
+SELECT ename FROM t_employee WHERE sal IS NULL;
+
+#非空查询
+SELECT ename FROM t_employee WHERE sal IS NOT NULL;
+
+#取反查询
+SELECT ename FROM t_employee WHERE NOT sal IS NULL;
