99155772544f39c6b2b949c16a08299a8329bb2a
DESKTOP-6POJJMP\kiky <huafeihua123xxx@gmail.com>
2017-10-18 08:13:37 +0800

    排序查询

--- ch10/04--queryWithOrder.sql
+++ ch10/04--queryWithOrder.sql
@@ -13,3 +13,7 @@ SELECT * FROM t_employee ORDER BY sal;
 #单字段排序查询
 # mgr降序
 SELECT * FROM t_employee ORDER BY mgr DESC;
+
+#多字段排序
+#主序：sal升序	次序：hiredate 降序
+SELECT * FROM t_employee ORDER BY sal ASC,Hiredate DESC;
