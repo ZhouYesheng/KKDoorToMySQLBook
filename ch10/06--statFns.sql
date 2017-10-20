092fb7edf8352f9900b308a222152f0a9523cc3a
DESKTOP-6POJJMP\kiky <huafeihua123xxx@gmail.com>
2017-10-21 06:50:57 +0800

    无记录统计查询

--- ch10/06--statFns.sql
+++ ch10/06--statFns.sql
@@ -21,7 +21,7 @@ SELECT AVG(comm) AS aver FROM t_employee
 #求和查询
 SELECT SUM(sal) AS allSals FROM t_employee
 	WHERE NOT comm=0;
-	
+
 #极值查询
 SELECT MAX(sal) AS maxSal,
 	MIN(sal) AS minSal FROM t_employee;
@@ -34,3 +34,12 @@ SELECT MAX(comm) AS maxComm,
 SELECT MAX(comm) AS maxComm,
 	MIN(comm) AS minComm FROM t_employee
 	WHERE NOT comm=0;
+
+#无记录查询——count返回0
+SELECT COUNT(*) FROM t_dept;
+
+#无记录查询——非count都返回null
+SELECT AVG(deptno) AS avgNo,
+	SUM(deptno) AS sumNo,
+	MAX(deptno) AS maxNo,
+	MIN(deptno) AS minNO FROM t_dept;
