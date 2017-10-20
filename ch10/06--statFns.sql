0d80d38be02eb85aafde60d435981ff4b1abac72
DESKTOP-6POJJMP\kiky <huafeihua123xxx@gmail.com>
2017-10-21 06:13:16 +0800

    均值查询AVG

--- ch10/06--statFns.sql
+++ ch10/06--statFns.sql
@@ -10,3 +10,9 @@ SELECT COUNT(*) FROM t_employee;
 #查询符合条件的记录数
 SELECT COUNT(comm) AS number FROM t_employee
 	WHERE NOT comm=0;
+
+#均值查询
+SELECT AVG(comm) AS aver FROM t_employee;
+
+SELECT AVG(comm) AS aver FROM t_employee
+	WHERE NOT comm=0;
