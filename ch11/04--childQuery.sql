b41d4ffdebe69095634ff673862290de562c2a51
DESKTOP-6POJJMP\kiky <huafeihua123xxx@gmail.com>
2017-10-21 11:18:39 +0800

    多行多列子查询【作为主查询的条件的参考值】

--- ch11/04--childQuery.sql
+++ ch11/04--childQuery.sql
@@ -11,3 +11,10 @@ SELECT COUNT(*) FROM t_dept,t_employee;
 SELECT * FROM t_employee
 	WHERE sal >
 		(SELECT sal FROM t_employee WHERE ename='SMITH');
+
+#多行多列子查询【作为主查询的条件的参考值】
+#查询和SMITH的工资、岗位【子查询，员工表】
+#一样的所有员工【主查询，员工表】
+SELECT ename,sal,job FROM t_employee
+	WHERE (sal,job) =
+		(SELECT sal,job FROM t_employee WHERE ename='SMITH');
