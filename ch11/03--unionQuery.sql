daa007af6043b166955a68d10e8be8eb3019bb43
DESKTOP-6POJJMP\kiky <huafeihua123xxx@gmail.com>
2017-10-21 11:01:37 +0800

    UNION ALL合并查询

--- ch11/03--unionQuery.sql
+++ ch11/03--unionQuery.sql
@@ -8,3 +8,9 @@
 #和音乐系【t_mstudent】全部学生
 SELECT * FROM t_cstudent
 	UNION SELECT * FROM t_mstudent;
+
+#UNION ALL合并查询
+#查询计算机【t_cstduent】
+#和音乐系【t_mstudent】全部学生
+SELECT * FROM t_cstudent
+	UNION ALL SELECT * FROM t_mstudent;
