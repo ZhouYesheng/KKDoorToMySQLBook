47cae58b4b7f064eb806046057f45886b605730e
DESKTOP-6POJJMP\kiky <huafeihua123xxx@gmail.com>
2017-10-21 08:47:14 +0800

    学生M表

---
+++ ch11/00-mstudentTab.sql
@@ -0,0 +1,21 @@
+####
+#学生M表
+###
+#创建学生m表
+CREATE TABLE t_mstudent(
+	name VARCHAR(20),
+	sex CHAR(1)
+);
+
+#查询全部
+SELECT * FROM t_mstudent;
+
+#插入测试数据
+INSERT INTO t_mstudent
+	VALUES('ccjong','男'),
+		('ccjong1','女'),
+		('ccjong2','男'),
+		('ccjong3','男'),
+		('ccjong4','女'),
+		('ccjong5','女');
+
