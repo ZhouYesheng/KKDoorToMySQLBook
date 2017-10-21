bfcba1fdc07e4bec50f092baddada72a5fc3a03c
DESKTOP-6POJJMP\kiky <huafeihua123xxx@gmail.com>
2017-10-21 10:40:01 +0800

    右外连查询

--- ch11/02--outerJoin.sql
+++ ch11/02--outerJoin.sql
@@ -8,3 +8,12 @@ SELECT e.ename employeename,e.job,
 	l.ename loadername
 	FROM t_employee e
 	LEFT JOIN t_employee l ON e.mgr=l.empno;
+
+#右外连查询
+#查询员工姓名、职位【员工表】
+#关联部门的的名字和地址【部门表，部门编号相等】
+SELECT e.ename employeename,e.job,
+	d.dname,d.loc
+	FROM t_employee e
+	RIGHT JOIN t_dept d ON e.deptno=d.deptno;
+
