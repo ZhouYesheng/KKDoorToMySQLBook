3264a37907ca302187cba3b5aab076faa8c2522d
DESKTOP-6POJJMP\kiky <huafeihua123xxx@gmail.com>
2017-10-21 10:10:23 +0800

    内连等值查询

--- ch11/01--innerJoin.sql
+++ ch11/01--innerJoin.sql
@@ -27,3 +27,51 @@ SELECT e.empno,e.ename,e.job,
 		d.dname,d.loc
 		FROM t_employee e INNER JOIN t_dept d
 		ON e.deptno=d.deptno;
+
+#等值连接（where形式）
+#查询员工关联的部门
+SELECT e.empno,e.ename,e.job,
+		d.dname,d.loc
+		FROM t_employee e,t_dept d
+		WHERE e.deptno=d.deptno;
+
+#查询员工的编号、姓名、工资和职位
+SELECT e.empno,ename employeename,e.sal,e.job
+	FROM t_employee e;
+
+#查询员工的编号、姓名、工资和职位、领导姓名
+SELECT e.empno,e.ename employeename,e.sal,e.job,
+	l.ename loadername
+	FROM t_employee e INNER JOIN t_employee l
+	ON e.mgr=l.empno;
+
+#查询员工的编号、姓名、工资和职位、
+#领导名
+#部门信息
+SELECT e.empno,e.ename employeename,e.sal,e.job,
+	l.ename loadername,
+	d.dname,d.loc
+	FROM t_employee e
+	INNER JOIN t_employee l ON e.mgr=l.empno
+	INNER JOIN t_dept d ON l.deptno=d.deptno;
+
+#查询员工的编号、姓名、工资和职位、
+#领导名
+#部门信息(WHERE形式)
+SELECT e.empno,e.ename employeename,e.sal,e.job,
+	l.ename loadername,
+	d.dname,d.loc
+	FROM t_employee e,t_employee l,t_dept d
+	WHERE e.mgr=l.empno AND l.deptno=d.deptno;
+
+
+
+
+
+
+
+
+
+
+
+
