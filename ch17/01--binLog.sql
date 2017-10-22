7fbf2cdae088518d533b4b20374f1792381ed738
DESKTOP-6POJJMP\kiky <huafeihua123xxx@gmail.com>
2017-10-22 16:53:15 +0800

    删除二进制文件

--- ch17/01--binLog.sql
+++ ch17/01--binLog.sql
@@ -13,3 +13,13 @@ SET SQL_LOG_BIN=1;
 
 #打开日志后，会被记录
 DROP TABLE t_loader;
+
+
+#删除所有二进制日志文件
+RESET MASTER;
+
+#删除指定索引之前的所有二进制日志
+PURGE MASTER LOGS TO 'DESKTOP-6POJJMP-bin.000003';
+
+#删除指定日期之前的所有日志
+PURGE MASTER LOGS BEFORE '2017-10-22 16:50:00';
