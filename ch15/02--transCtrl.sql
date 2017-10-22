6f67f093d1d6e767f9349efb6e85485ca0340c13
DESKTOP-6POJJMP\kiky <huafeihua123xxx@gmail.com>
2017-10-22 09:03:58 +0800

    事务控制

--- ch15/02--transCtrl.sql
+++ ch15/02--transCtrl.sql
@@ -8,3 +8,18 @@ BEGIN;
 	UPDATE test_1 SET username='test2' WHERE id=1;
 #提交事务
 COMMIT;
+
+#开启事务
+BEGIN;
+#更新记录
+UPDATE test_1 SET username='test4' WHERE id=1;
+#不提交事务
+
+
+#开启事务
+BEGIN;
+#更新记录
+UPDATE test_1 SET username='test' WHERE id=1;
+#不提交事务
+#回滚事务
+ROLLBACK;
